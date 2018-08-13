a0fb4a4fcce66e33165f0e86d04baca60ab88dd1
litian1045 <litian1045@gmail.com>
2018-08-13 14:29:32 +1200

    'test'

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas
+
+
+apl_tr = pandas.read_csv('Data/application_train.csv')
+cols = apl_tr.columns
+apl_tr_dict = list()
+for col in cols:
+    apl_tr_dict += [{
+        'col_name': col,
+        'type': apl_tr[col].dtypes,
+        'length': len(apl_tr[col]),
+        'NAs': sum(pandas.isna(apl_tr[col]))
+    }]
+print(apl_tr_dict)
+
